7b53216e7f2d605d7b142bd1908f7c1154533eb0
Aditya Parihar <adityapariharparihar@gmail.com>
2023-07-18 21:26:01 +0530

    int length = 1 << (n - 1); // Calculate length as 2^(n-1)         if (k <= length / 2) {             return kthGrammar(n - 1, k);         } else {             return 1 - kthGrammar(n - 1, k - length / 2);         }         ⚠️

--- KthBit.java
+++ KthBit.java
@@ -1,6 +1,6 @@
 public class KthBit {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(findKthGrammar(2, 2));
     }
 
     char findKthBit(int n, int k) {
@@ -16,6 +16,19 @@ public class KthBit {
         } else {
             char prevBit = findKthBit(n - 1, length - k + 1);
             return prevBit == '0' ? '1' : '0';
+        }
+    }
+
+    static int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int length = 1 << (n - 1); // Calculate length as 2^(n-1)
+        if (k <= length / 2) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return 1 - kthGrammar(n - 1, k - length / 2);
         }
     }
 
