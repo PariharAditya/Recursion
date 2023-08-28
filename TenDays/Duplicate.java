8656c5298320ecf32fefa1a702d6bf568c02de4d
Aditya Parihar <adityapariharparihar@gmail.com>
2023-08-28 16:12:23 +0530

    1/4

---
+++ TenDays/Duplicate.java
@@ -0,0 +1,21 @@
+package TenDays;
+
+import java.util.Scanner;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicate("aaabc"));          
+    }
+    static String removeDuplicate(String original) {
+        if (original.length() <= 1) {
+            return original;
+        }
+        char c = original.charAt(0);
+
+        if (c != original.charAt(1)) {
+            return c + removeDuplicate(original.substring(1));
+        } else {
+            return removeDuplicate(original.substring(1));
+        }
+    }
+}
